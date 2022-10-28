5cd149c24878578526cf34e3832bf213737e832e
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2022-10-28 16:54:01 -0300

    add blogDetails

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,24 +5,22 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not fetch the data for that resource.");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1500);
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+      });
   }, [url]);
 
   return { data, isPending, error };
